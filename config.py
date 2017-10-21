cf830bbc920dac158e7c83d8d0f8606c97d42161
Cryptoad <chrisjohnmichels@gmail.com>
2017-10-21 16:48:07 -0400

    secret though

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
-KEY = "f99b109757d131f6757c69ea02a0e960"
-SECRET = "rJuRor/TtqK7P2ayqPTfmXi5tVKo/QoUtWwrnkZo0cIR+zgAoly7myCn2REdGa3XdZZBsLfTjyPj/o08sol8xA=="
-PASSPHRASE = "m1dkgamlc8"
+KEY = ""
+SECRET = ""
+PASSPHRASE = ""
 LIVE = False
 FRONTEND = "curses"
